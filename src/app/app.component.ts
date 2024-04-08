c6d6bd9ca7396c99e352dc50104174e064f9ef03
margotr <github@margotrutgers.nl>
2024-04-08 19:58:04 +0200

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink} from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CvComponent } from './cv/cv.component';
+import { routes } from './app.routes';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, RouterLink]
 })
 export class AppComponent {
   title = 'margot-home';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CvComponent } from './cv/cv.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'cv', component: CvComponent },
+];
